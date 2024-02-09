04c6b667a1fe7943b70b747d5fa2f8261d2c74e2
lham-ss <lham@staffsetter.com>
2024-02-09 13:27:35 -0700

    add boilerplate basics

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 // TODO: express server microservice
+// lham@ss.io
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5050;
+
+app.get('/', (req, res) => {
+    res.send('Yes, of course it is working...');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
+
