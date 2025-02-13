1dda5d742d67c77c167de0e7f9cf4b4dfa622494
lham-ss <lham@staffsetter.io>
2025-02-13 13:48:22 -0700

    save

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const pngImageFactory = require('./micr-to-png'); // gonna make you sweat!
 
 const app = express();
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3031;
 
 const defaultRoutingNumber = '121000248';    // use this default if no routing number is provided
 const defaultAccountNumber = '4942600000';  // use this default if no account number is provided
@@ -26,6 +26,35 @@ app.get('/api/rcp-solutions/micr-gen', (req, res) => {
         res.send(buf);
     }
 });
+
+app.get('/api/rcp-solutions/logo', (req, res) => {
+    const imagePath = this.path.join(__dirname, 'images', 'company_watermark.png');
+
+    fs.readFile(imagePath, (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Error loading image');
+        }
+    
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(data);
+      });
+});
+
+app.get('/api/rcp-solutions/logo', (req, res) => {
+    const imagePath = this.path.join(__dirname, 'images', 'new_check_signature.png');
+
+    fs.readFile(imagePath, (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Error loading image');
+        }
+    
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(data);
+      });
+});
+
 
 app.listen(port, () => {
     console.log(`MICR line image factory is running on port ${port}.`);
